b551810a3f59c857276576af70911121ceccee2f
Ed <ed321@hotmail.co.uk>
2022-08-04 15:58:12 +0100

    Added traits to Adapters

--- src/main/scala/payment/PaymentAdapter.scala
+++ src/main/scala/payment/PaymentAdapter.scala
@@ -4,7 +4,11 @@ import scala.util.Random
 
 class Payment(val amount: Double, val success: Boolean)
 
-object PaymentAdapter {
+trait PaymentAdapterBase {
+  def makePayment(amount:Double, onSuccess: (Payment) => Any, onFailure: (Payment) => Any): Unit
+}
+
+object PaymentAdapter extends PaymentAdapterBase {
   def makePayment(amount:Double, onSuccess: (Payment) => Any, onFailure: (Payment) => Any) = {
     if(Random.nextInt(10) >= 8) {
       onSuccess(new Payment(amount, false))
