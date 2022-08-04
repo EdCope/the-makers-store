4a3891560366c6862a30f50138cc6a34fbd7b858
Ed <ed321@hotmail.co.uk>
2022-08-04 15:55:23 +0100

    Test written but needs adjusting for async drop the table

--- src/main/scala/controller/ItemController.scala
+++ src/main/scala/controller/ItemController.scala
@@ -1,12 +1,25 @@
 package controller
 
 import main.model.Item
-
 import scala.collection.mutable.ArrayBuffer
-
 
 class ItemController( ) {
   def fetchAll(): ArrayBuffer[Item] = {
     main.db.DbAdapter.getItems()
   }
+
+  def create(name: String, price: Double, quantity: Integer, availableLocales: List[String]): Unit = {
+//    {
+//      "id": 7,
+//      "name": "Orange Peel",
+//      "price": 0.4,
+//      "quantity": 8,
+//      "availableLocales": ["EU", "UK"]
+//    }
+    println(next_id)
+    val item = new Item(next_id, name, price, quantity, availableLocales)
+    main.db.DbAdapter.createItem(item)
+  }
+
+  private def next_id:Int = fetchAll().last.id + 1
 }
--- src/test/scala/controller/ItemControllerTest.scala
+++ src/test/scala/controller/ItemControllerTest.scala
@@ -1,17 +1,30 @@
 package controller
 
-import main.model.Item
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalamock.scalatest.MockFactory
 
 import scala.collection.mutable.ArrayBuffer
 
-class ItemControllerTest extends AnyWordSpec with Matchers {
+class ItemControllerTest extends AnyWordSpec with Matchers with MockFactory {
   "An Item Controller" should {
+    main.db.DbAdapter.dropAndReset
+    val subject = new ItemController
     "have a fetchAll method" which{
       "returns all the items" in {
-        val item = new ItemController
-        item.fetchAll shouldBe a[ArrayBuffer[Item]]
+        subject.fetchAll shouldBe a[ArrayBuffer[_]]
+      }
+    }
+    "has a create method" which {
+      "adds an item to the end" in {
+        val obj = subject.fetchAll()
+        val id = obj.last.id + 1
+        subject.create("Egg", 0.5, 4, List("EU"))
+        obj.last.id shouldBe id
+        obj.last.name shouldBe "Egg"
+        obj.last.price shouldBe 0.5
+        obj.last.quantity shouldBe 4
+        obj.last.availableLocales shouldEqual List("EU")
       }
     }
   }
