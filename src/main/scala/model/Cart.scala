64535e5410453bd52f3637b39d34c3e3d9165ef3
Ed <ed321@hotmail.co.uk>
2022-08-11 18:03:28 +0100

    Cart has a location

--- src/main/scala/model/Cart.scala
+++ src/main/scala/model/Cart.scala
@@ -1,9 +1,10 @@
 package model
 
-import main.model.Item
+import main.model.{Item, Location}
+
 import scala.collection.mutable.ArrayBuffer
 
-class Cart (val uuidFactory: factory.UUIDFactoryBase = factory.UUIDFactory,
+class Cart (val location: Location, val uuidFactory: factory.UUIDFactoryBase = factory.UUIDFactory,
             val contents: ArrayBuffer[Item] = ArrayBuffer()) {
   val uuid = uuidFactory.create()
 
--- src/test/scala/model/CartTest.scala
+++ src/test/scala/model/CartTest.scala
@@ -1,6 +1,6 @@
 package model
 import factory.UUIDFactoryBase
-import main.model.Item
+import main.model.{Item, Location}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalamock.scalatest.MockFactory
@@ -8,22 +8,31 @@ import org.scalamock.scalatest.MockFactory
 import scala.collection.mutable.ArrayBuffer
 
 class CartTest extends AnyWordSpec with Matchers with MockFactory {
-  "Cart Model" should{
-     "Cart should have a uuid" in  {
-       val mockUUIDFactory = mock[UUIDFactoryBase]
-       (mockUUIDFactory.create _).expects().returning("123e4567-e89b-12d3-a456-426655440000")
-      val cart = new Cart(mockUUIDFactory)
-      cart.uuid shouldEqual "123e4567-e89b-12d3-a456-426655440000"
+  "Cart Model" should {
+    val cartLocation = new Location(10, "Test Town")
+    "CartAttributes are" which {
+      "have a uuid" in  {
+        val mockUUIDFactory = mock[UUIDFactoryBase]
+        (mockUUIDFactory.create _).expects().returning("123e4567-e89b-12d3-a456-426655440000")
+        val cart = new Cart(cartLocation, mockUUIDFactory)
+        cart.uuid shouldEqual "123e4567-e89b-12d3-a456-426655440000"
+      }
+      "has a collection of items" in {
+        val cart = new Cart(cartLocation)
+        cart.contents shouldBe a [ArrayBuffer[_]]
+      }
+      "a cart should be at a location" in {
+        val cart = new Cart(cartLocation)
+        cart.location shouldBe cartLocation
+      }
     }
-    "has a collection of items" in {
-      val cart = new Cart()
-      cart.contents shouldBe a [ArrayBuffer[_]]
-    }
+
     "can add an Item" in {
-      val cart = new Cart()
+      val cart = new Cart(cartLocation)
       val sampleItem = new Item(5, "Egg", 0.2, 6, List("EU"))
       cart.addItem(sampleItem)
       cart.contents shouldEqual ArrayBuffer(sampleItem)
     }
+
   }
 }
